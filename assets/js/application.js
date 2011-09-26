ba576282ec2074a83dc9e7875a72a6bc39415274
Jonathan Dahan <jonathan@jedahan.com>
2011-09-26 13:56:08 -0400

    markdown2prettyprint

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,15 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	// Google code prettify
-	prettyPrint();
+  // add prettyprint class to all <pre><code></code></pre> blocks
+  var prettify = false;
+  $("pre code").parent().each(function() {
+      $(this).addClass('prettyprint');
+      prettify = true;
+  });
 
-	// Dropdown mouseover
-	$(".nav-dropdown").mouseover(function () {
-		$(".nav-dropdown").toggleClass('open');
-	});
+  // if code blocks were found, bring in the prettifier ...
+  if ( prettify ) {
+      $.getScript("/js/prettify.js", function() { prettyPrint() });
+  }
 
-	$(".nav-dropdown").mouseout(function () {
-		$(".nav-dropdown").toggleClass('open');
-	});
+  // Instantiate prettyprint
+  prettyPrint();
 
+  // Dropdown mouseover
+  $(".nav-dropdown").mouseover(function () {
+    $(".nav-dropdown").toggleClass('open');
+  });
+
+  $(".nav-dropdown").mouseout(function () {
+    $(".nav-dropdown").toggleClass('open');
+  });
 });
