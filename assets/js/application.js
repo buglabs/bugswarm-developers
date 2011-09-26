072f2f02e21a36ac0a43c813b454f513828c81a2
Jonathan Dahan <jonathan@jedahan.com>
2011-09-26 15:17:29 -0400

    always prettyprint

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
 
   // add prettyprint class to all <pre><code></code></pre> blocks
-  var prettify = false;
   $("pre code").parent().each(function() {
       $(this).addClass('prettyprint');
-      prettify = true;
   });
-
-  // if code blocks were found, bring in the prettifier ...
-  if ( prettify ) {
-      $.getScript("/js/prettify.js", function() { prettyPrint() });
-  }
 
   // Instantiate prettyprint
   prettyPrint();
