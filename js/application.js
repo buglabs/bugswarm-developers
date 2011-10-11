230767b500eaae93c777e09d2f01e0ced5cb46e6
Davis Barber <davis.barber@buglabs.net>
2011-10-10 21:18:49 -0400

    regenerated docs

---
+++ js/application.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  // Instantiate prettyprint
+  prettyPrint();
+
+  // Dropdown mouseover
+  $(".nav-dropdown").mouseover(function () {
+    $(".nav-dropdown").toggleClass('open');
+  });
+
+  $(".nav-dropdown").mouseout(function () {
+    $(".nav-dropdown").toggleClass('open');
+  });
+});
