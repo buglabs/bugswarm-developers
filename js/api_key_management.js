fb83c9e158de2feaee70ef7f88c8d5118bdc5580
Davis Barber <davis.barber@buglabs.net>
2011-10-12 11:57:14 -0400

    adds api_key_management.html

---
+++ js/api_key_management.js
@@ -0,0 +1,72 @@
+	<script language="javascript" type="text/javascript">
+//<![CDATA[
+   function supports_html5_storage() {
+        try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+    return false;
+    }
+    }
+
+    function createKeys(){
+       if (supports_html5_storage()){
+               localStorage.setItem("bugswarm_username", document.account_fields.login.value);
+               localStorage.setItem("bugswarm_password", document.account_fields.pass.value);
+            }
+            postCORS('http://api.bugswarm.net/keys', document.account_fields.login.value, document.account_fields.pass.value, function(data){showConfKeyPost(data)});
+            postCORS('http://api.bugswarm.net/keys/participation', document.account_fields.login.value, document.account_fields.pass.value, function(data){showPartKeyPost(data)}); 
+     }
+     function getKeys(){
+     	getCORS('http://api.bugswarm.net/keys', document.account_fields.login.value, document.account_fields.pass.value, function(data){showKeysGet(data)});
+     }
+     
+     function showKeysGet(data){
+     	console.log(JSON.stringify(data));
+     	for (var i = 0; i < data.length; i++) {
+     	    if (data[i].type == 'configuration'){
+     	    	document.api_key_fields.configuration.value = data[i].key.replace(/['"]/g,'');
+     	    }
+     	    else if (data[i].type == 'participation'){
+     	    	document.api_key_fields.participation.value = data[i].key.replace(/['"]/g,'');
+     	    }
+		}
+     }
+        
+     function showConfKeyPost(data){
+     	console.log(JSON.stringify(data));
+     	document.api_key_fields.configuration.value = JSON.stringify(data.apikey).replace(/['"]/g,'');
+     }
+     function showPartKeyPost(data){
+     	console.log(JSON.stringify(data));
+     	document.api_key_fields.participation.value = JSON.stringify(data.apikey).replace(/['"]/g,'');
+     }
+        
+     function postCORS(url, username, password, callback, type){
+		$.ajax({
+		url : url,
+	    type : 'POST',
+	    headers: {'Authorization': 'Basic ' +Base64.encode(username + ":" + password)},
+			success: callback
+		});
+	 }
+     function getCORS(url, username, password, callback, type){
+		$.ajax({
+		url : url,
+	    type : 'GET',
+	    headers: {'Authorization': 'Basic ' +Base64.encode(username + ":" + password)},
+			success: callback
+		});
+	 }  
+    
+     function loadValues(){
+		if (supports_html5_storage()){
+			if (typeof(localStorage["bugswarm_username"])!="undefined"){
+	        		document.account_fields.login.value = localStorage["bugswarm_username"];
+			}
+			if (typeof(localStorage["bugswarm_password"])!="undefined"){
+	        		document.account_fields.pass.value = localStorage["bugswarm_password"];
+			}
+        	}
+	}
+    //]]>
+    </script>
