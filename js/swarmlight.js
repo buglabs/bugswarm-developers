ba2938305b9f3301c5bd0d0eeb6d8054bb55ad4e
barberdt <barberdt@gmail.com>
2011-10-17 15:52:57 -0400

    fixes apikey problem with swarmlight.js

--- js/swarmlight.js
+++ js/swarmlight.js
@@ -1,4 +1,4 @@
-SWARM.connect({apikey: '2e03487c7fb0c78760de1b80514f79f1ccdfcb4b',
+SWARM.connect({apikey: 'a35c8276f241a967d8bdf59a07d4b5d522447b17',
                resource: '0e3c838394a77cff63f008d9c36f11a1c5921eee',
                swarms: ['d0e0de97f0b1ebe17762654f209b3bd100de6bf6'],
                onmessage: function() {},
