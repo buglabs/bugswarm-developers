e5f919acf63416c13d15b99b51f252bf6d523731
barberdt <barberdt@gmail.com>
2011-10-19 16:09:25 -0400

    regenerated docs

--- js/swarmlight.js
+++ js/swarmlight.js
@@ -1,5 +1,5 @@
 SWARM.connect({apikey: 'a35c8276f241a967d8bdf59a07d4b5d522447b17',
-               resource: '0e3c838394a77cff63f008d9c36f11a1c5921eee',
+               resource: '971abb0d2ec4807248f700f7e139dbbf1ca03d25',
                swarms: ['d0e0de97f0b1ebe17762654f209b3bd100de6bf6'],
                onmessage: function() {},
                onpresence: function() {},
